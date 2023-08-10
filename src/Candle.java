3add9fb44aa03227dd8ea5fd1409988f1382c185
abraham <abrahamvarguez21@yahoo.com>
2023-08-10 10:17:23 -0400

    final

--- src/Candle.java
+++ src/Candle.java
@@ -4,12 +4,16 @@ public class Candle {
     public int amount;
     public double price;
 
+
+    //A constructor method to create the variables needed to be printed on to the receipt
     public Candle(String name, int amount, double price) {
         this.name = name;
         this.amount = amount;
         this.price = price;
     }
 
+
+    //The method created to actually print the variables for the receipt that we will call in the CandleShopReceipt class
     public void printReceipt() {
         System.out.println();
         System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
